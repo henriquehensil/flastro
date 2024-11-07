2163a04741f7bbf20509ff9d3e74569160319b9c
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-07 16:22:57 -0300

    improved code

--- src/main/java/ghostface/dev/datatype/DataType.java
+++ src/main/java/ghostface/dev/datatype/DataType.java
@@ -1,5 +1,6 @@
 package ghostface.dev.datatype;
 
+import ghostface.dev.exception.DataException;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -7,8 +8,8 @@ import java.io.InputStream;
 
 public interface DataType<T> {
 
-    @NotNull T read(@NotNull InputStream stream) throws IOException;
+    @NotNull T read(@NotNull InputStream stream) throws IOException, DataException;
 
-    @NotNull T read(byte @NotNull [] bytes);
+    @NotNull T read(byte @NotNull [] bytes) throws DataException;
 
 }

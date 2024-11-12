60cd8a91d847a2643c9da8af9155fd336e64fcfb
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-11 21:51:27 -0300

    add #getValues

--- src/main/java/ghostface/dev/table/data/Data.java
+++ src/main/java/ghostface/dev/table/data/Data.java
@@ -6,8 +6,10 @@ import ghostface.dev.table.column.Column;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnknownNullability;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 public interface Data extends Serializable {
 
@@ -16,5 +18,7 @@ public interface Data extends Serializable {
     <E> @UnknownNullability E get(@NotNull Column<E> column) throws IllegalArgumentException;
 
     <E> void set(@NotNull Column<E> column, @Nullable E value) throws ColumnException, IllegalArgumentException;
+
+    @Unmodifiable @NotNull Collection<@Nullable Object> getValues();
 
 }

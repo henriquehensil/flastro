1a1fc0d39d3d95d4e8cbc9787f7f00b370653e2a
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-11 22:11:48 -0300

    add #getName and #setName

--- src/main/java/ghostface/dev/table/column/Column.java
+++ src/main/java/ghostface/dev/table/column/Column.java
@@ -1,4 +1,4 @@
-package ghostface.dev.storage.table.column;
+package ghostface.dev.table.column;
 
 import ghostface.dev.datatype.DataType;
 import org.jetbrains.annotations.NotNull;
@@ -7,12 +7,13 @@ public interface Column<T> {
 
     @NotNull String getName();
 
+    void setName(@NotNull String name);
+
     @NotNull DataType<T> getDataType();
 
     boolean isKey();
 
     boolean isNullable();
 
-    void setName(@NotNull String name);
 
 }

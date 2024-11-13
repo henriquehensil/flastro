1090cf33ed3979636753b4aae4f0883498b962fd
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-13 20:32:15 -0300

    added Key

---
+++ src/main/java/ghostface/dev/table/Key.java
@@ -0,0 +1,12 @@
+package ghostface.dev.table;
+
+import ghostface.dev.table.column.Column;
+import org.jetbrains.annotations.NotNull;
+
+public interface Key<T> {
+
+    @NotNull Column<T> getColumn();
+
+    @NotNull T getValue();
+
+}

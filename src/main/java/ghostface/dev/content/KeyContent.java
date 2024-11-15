f54663f3db859fcb63f6ce5123f086f0c0dc5b6e
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-15 16:16:17 -0300

    defined Content

---
+++ src/main/java/ghostface/dev/content/KeyContent.java
@@ -0,0 +1,18 @@
+package ghostface.dev.content;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface KeyContent<K, V> {
+
+    @NotNull Optional<? extends V> get(@NotNull K id);
+
+    boolean put(@NotNull K id, @NotNull V value);
+
+    boolean delete(@NotNull K id);
+
+    @Unmodifiable @NotNull Collection<? extends V> toCollection();
+}

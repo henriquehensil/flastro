bd1fd67d749af15d4ef867ba45c8837260850f75
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-20 18:25:22 -0300

    renamed Data to Elements

--- src/main/java/ghostface/dev/table/Table.java
+++ src/main/java/ghostface/dev/table/Table.java
@@ -2,14 +2,14 @@ package ghostface.dev.table;
 
 import ghostface.dev.database.Database;
 import ghostface.dev.table.column.Columns;
-import ghostface.dev.table.data.Datas;
+import ghostface.dev.table.data.Elements;
 import org.jetbrains.annotations.NotNull;
 
 public interface Table {
 
     @NotNull Database getDatabase();
 
-    @NotNull Datas getElements();
+    @NotNull Elements getElements();
 
     @NotNull Columns getColumns();
 }
--- src/main/java/ghostface/dev/table/data/Elements.java
+++ src/main/java/ghostface/dev/table/data/Elements.java
@@ -1,5 +1,6 @@
 package ghostface.dev.table.data;
 
+import ghostface.dev.exception.column.DuplicatedColumnException;
 import ghostface.dev.exception.key.DuplicatedKeyException;
 import ghostface.dev.exception.key.MissingKeyException;
 import ghostface.dev.exception.table.TableException;
@@ -11,14 +12,15 @@ import org.jetbrains.annotations.Unmodifiable;
 import java.util.Collection;
 import java.util.Optional;
 
-public interface Datas {
+public interface Elements {
 
     /**
      * @throws MissingKeyException if the key is missing
      * @throws DuplicatedKeyException if the key value already exists
      * @throws TableException if the key's column does not belong to the table.
+     * @throws DuplicatedColumnException if the keys contain duplicate columns.
      * */
-    @NotNull Data create(@NotNull Key<?> @NotNull ... keys) throws MissingKeyException, DuplicatedKeyException, TableException;
+    @NotNull Data create(@NotNull Key<?> @NotNull ... keys) throws MissingKeyException, DuplicatedKeyException, DuplicatedColumnException, TableException;
 
     boolean remove(int index);
 

7833aac0e9892c231073e655c1a9cba1049feb62
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-26 23:51:11 -0300

    improved system

--- src/main/java/ghostface/dev/table/column/Columns.java
+++ src/main/java/ghostface/dev/table/column/Columns.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
-import ghostface.dev.exception.NameAlreadyExists;
+import ghostface.dev.exception.NameAlreadyExistsException;
 import ghostface.dev.exception.table.TableStateException;
 import java.util.Collection;
 import java.util.Optional;
@@ -15,21 +15,21 @@ import java.util.Optional;
 public interface Columns extends UnmodifiableContent<@NotNull Column<?>> {
 
     /**
-     * @throws NameAlreadyExists if name is already in use
+     * @throws NameAlreadyExistsException if name is already in use
      * */
-    <E> @NotNull Column<E> create(@NotNull String name, @NotNull DataType<E> dataType, @Nullable E value, boolean isNullable) throws NameAlreadyExists;
+    <E> @NotNull Column<E> create(@NotNull String name, @NotNull DataType<E> dataType, @Nullable E value, boolean isNullable) throws NameAlreadyExistsException;
 
     /**
-     * @throws NameAlreadyExists if the name is already in use
+     * @throws NameAlreadyExistsException if the name is already in use
      * @throws TableStateException if the table elements is not empty
      * */
-    <E> @NotNull Column<E> createKey(@NotNull String name, @NotNull DataType<E> dataType) throws NameAlreadyExists, TableStateException;
+    <E> @NotNull Column<E> createKey(@NotNull String name, @NotNull DataType<E> dataType) throws NameAlreadyExistsException, TableStateException;
 
     // Getters
 
     @NotNull Table getTable();
 
-    @NotNull Optional<? extends Column<?>> get(@NotNull String name);
+    <E> @NotNull Optional<? extends Column<E>> get(@NotNull String name, @NotNull DataType<E> dataType);
 
     @Override
     @Unmodifiable @NotNull Collection<? extends @NotNull Column<?>> toCollection();

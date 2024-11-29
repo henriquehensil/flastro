a934d645d4a650f42515613bc1509956250f0b19
Henrique Gustavo <henriquedev6@gmail.com>
2024-11-29 14:21:54 -0300

    removed AuthenticationImpl

--- src/main/java/ghostface/dev/database/Authentication.java
+++ src/main/java/ghostface/dev/database/Authentication.java
@@ -4,13 +4,72 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 
 public interface Authentication {
 
+    static @NotNull Authentication create(
+            @NotNull InetAddress address,
+            @Range(from = 0, to = 65535) int port,
+            @NotNull String username,
+            @NotNull String password
+    ) {
+        return new Authentication() {
+            @Override
+            public @NotNull InetAddress getAddress() {
+                return address;
+            }
+
+            @Override
+            public @Range(from = 0, to = 65535) int getPort() {
+                return port;
+            }
+
+            @Override
+            public @NotNull String getUsername() {
+                return username;
+            }
+
+            @Override
+            public @NotNull String getPassword() {
+                return password;
+            }
+        };
+    }
+
+    static @NotNull Authentication create(
+            @NotNull InetSocketAddress address,
+            @NotNull String username,
+            @NotNull String password
+    ) {
+        return new Authentication() {
+            @Override
+            public @NotNull InetAddress getAddress() {
+                return address.getAddress();
+            }
+
+            @Override
+            public @Range(from = 0, to = 65535) int getPort() {
+                return address.getPort();
+            }
+
+            @Override
+            public @NotNull String getUsername() {
+                return username;
+            }
+
+            @Override
+            public @NotNull String getPassword() {
+                return password;
+            }
+        };
+    }
+
+    // Objects
+
     @NotNull InetAddress getAddress();
 
-    @Range(from = 0, to = 65535)
-    int getPort();
+    @Range(from = 0, to = 65535) int getPort();
 
     @NotNull String getUsername();
 

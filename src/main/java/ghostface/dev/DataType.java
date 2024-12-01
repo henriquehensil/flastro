0ed24f9db502f4b46663cf6a60822c1d082e6f71
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-01 13:48:51 -0300

    added #write

--- src/main/java/ghostface/dev/DataType.java
+++ src/main/java/ghostface/dev/DataType.java
@@ -1,6 +1,5 @@
 package ghostface.dev;
 
-import ghostface.dev.exception.DataTypeException;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
@@ -9,11 +8,17 @@ public interface DataType<T> {
 
     @NotNull DataType<String> STRING = new DataType<String>() {
         @Override
-        public @NotNull String read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull String read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readUTF();
-            } catch (EOFException | UTFDataFormatException e) {
-                throw new DataTypeException("No String found");
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull String data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeUTF(data);
+                writer.flush();
             }
         }
 
@@ -22,14 +27,19 @@ public interface DataType<T> {
             return String.class;
         }
     };
-
     @NotNull DataType<Byte> BYTE = new DataType<Byte>() {
         @Override
-        public @NotNull Byte read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Byte read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readByte();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Byte found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Byte data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeByte(data);
+                writer.flush();
             }
         }
 
@@ -38,14 +48,19 @@ public interface DataType<T> {
             return Byte.class;
         }
     };
-
     @NotNull DataType<Short> SHORT = new DataType<Short>() {
         @Override
-        public @NotNull Short read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Short read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readShort();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Short found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Short data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeShort(data);
+                writer.flush();
             }
         }
 
@@ -54,14 +69,19 @@ public interface DataType<T> {
             return Short.class;
         }
     };
-
     @NotNull DataType<Integer> INTEGER = new DataType<Integer>() {
         @Override
-        public @NotNull Integer read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Integer read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readInt();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Integer found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Integer data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeInt(data);
+                writer.flush();
             }
         }
 
@@ -70,14 +90,19 @@ public interface DataType<T> {
             return Integer.class;
         }
     };
-
     @NotNull DataType<Long> LONG = new DataType<Long>() {
         @Override
-        public @NotNull Long read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Long read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readLong();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Long found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Long data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeLong(data);
+                writer.flush();
             }
         }
 
@@ -86,14 +111,19 @@ public interface DataType<T> {
             return Long.class;
         }
     };
-
     @NotNull DataType<Float> FLOAT = new DataType<Float>() {
         @Override
-        public @NotNull Float read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Float read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readFloat();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Float found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Float data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeFloat(data);
+                writer.flush();
             }
         }
 
@@ -102,14 +132,19 @@ public interface DataType<T> {
             return Float.class;
         }
     };
-
     @NotNull DataType<Double> DOUBLE = new DataType<Double>() {
         @Override
-        public @NotNull Double read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Double read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readDouble();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Double found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Double data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeDouble(data);
+                writer.flush();
             }
         }
 
@@ -118,14 +153,19 @@ public interface DataType<T> {
             return Double.class;
         }
     };
-
     @NotNull DataType<Boolean> BOOLEAN = new DataType<Boolean>() {
         @Override
-        public @NotNull Boolean read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Boolean read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readBoolean();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Boolean found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Boolean data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeBoolean(data);
+                writer.flush();
             }
         }
 
@@ -134,14 +174,19 @@ public interface DataType<T> {
             return Boolean.class;
         }
     };
-
     @NotNull DataType<Character> CHAR = new DataType<Character>() {
         @Override
-        public @NotNull Character read(@NotNull InputStream stream) throws DataTypeException, IOException {
+        public @NotNull Character read(@NotNull InputStream stream) throws IOException {
             try (@NotNull DataInputStream data = new DataInputStream(stream)) {
                 return data.readChar();
-            } catch (EOFException e) {
-                throw new DataTypeException("No Character found", e);
+            }
+        }
+
+        @Override
+        public void write(@NotNull OutputStream outputStream, @NotNull Character data) throws IOException {
+            try (@NotNull DataOutputStream writer = new DataOutputStream(outputStream)) {
+                writer.writeChar(data);
+                writer.flush();
             }
         }
 
@@ -154,9 +199,14 @@ public interface DataType<T> {
     // Objects
 
     /**
-     * @throws DataTypeException if data is not a valid DataType
+     * @throws IOException if an I/O exception occurs while reading.
      * */
-    @NotNull T read(@NotNull InputStream stream) throws DataTypeException, IOException;
+    @NotNull T read(@NotNull InputStream inputStream) throws IOException;
+
+    /**
+     * @throws IOException if an I/O exception occurs while writing.
+     * */
+    void write(@NotNull OutputStream outputStream, @NotNull T data) throws IOException;
 
     @NotNull Class<T> getType();
 }

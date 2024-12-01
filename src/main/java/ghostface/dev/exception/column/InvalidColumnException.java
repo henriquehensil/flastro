82e595a8ad810abbd81f1f8f63b95b42ffdab94c
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-01 14:22:19 -0300

    added InvalidColumnException

---
+++ src/main/java/ghostface/dev/exception/column/InvalidColumnException.java
@@ -0,0 +1,7 @@
+package ghostface.dev.exception.column;
+
+public class InvalidColumnException extends IllegalArgumentException {
+    public InvalidColumnException(String s) {
+        super(s);
+    }
+}

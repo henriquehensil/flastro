eeee34b6213e6123c95a870b7c1518b3bafd69dd
alana <henriquesousaa37@gmail.com>
2024-12-06 16:51:21 -0300

    improved system

--- src/main/java/ghostface/dev/nest/Nest.java
+++ src/main/java/ghostface/dev/nest/Nest.java
@@ -2,7 +2,7 @@ package ghostface.dev.nest;
 
 import ghostface.dev.DataType;
 import ghostface.dev.content.UnmodifiableContent;
-import ghostface.dev.exception.NameAlreadyExistsException;
+import ghostface.dev.exception.nest.NestAlreadyExistsException;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Unmodifiable;
 
@@ -11,21 +11,30 @@ import java.util.Optional;
 
 public interface Nest<T> extends UnmodifiableContent<T> {
 
-    /**
-     * @throws NameAlreadyExistsException if the ID already in use
-     * */
-    <E> @NotNull Nest<E> createSub(@NotNull String id, @NotNull DataType<E> dataType) throws NameAlreadyExistsException;
-
-    boolean put(@NotNull String key, T value);
-
-    @NotNull NestStorage getNestStorage();
-
-    <E> @NotNull Optional<? extends Nest<E>> getSub(@NotNull String id, @NotNull DataType<E> dataType);
+    @NotNull NestStorage getStorage();
 
     @NotNull Optional<T> getValue(@NotNull String key);
+
+    boolean put(@NotNull String key, T value);
 
     @NotNull DataType<T> getDataType();
 
     @Override
     @Unmodifiable @NotNull Collection<T> toCollection();
+
+    // Classes
+
+    interface Children extends UnmodifiableContent<@NotNull Nest<?>> {
+
+        /**
+         * @throws NestAlreadyExistsException if the ID already in use
+         * */
+        <E> @NotNull Nest<E> createSub(@NotNull String id, @NotNull DataType<E> dataType) throws NestAlreadyExistsException;
+
+        @NotNull Optional<? extends @NotNull Nest<?>> getSub(@NotNull String id);
+
+        @Override
+        @Unmodifiable @NotNull Collection<@NotNull Nest<?>> toCollection();
+
+    }
 }

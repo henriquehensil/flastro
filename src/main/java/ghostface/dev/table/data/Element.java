b513d7e99fa7c91b9bac52ebece5daa3c130a549
alana <henriquesousaa37@gmail.com>
2024-12-09 23:55:07 -0300

    improved code

--- src/main/java/ghostface/dev/table/data/Element.java
+++ src/main/java/ghostface/dev/table/data/Element.java
@@ -3,25 +3,24 @@ package ghostface.dev.table.data;
 import ghostface.dev.content.UnmodifiableContent;
 import ghostface.dev.exception.column.ColumnException;
 import ghostface.dev.exception.column.InvalidColumnException;
-import ghostface.dev.table.Data;
+import ghostface.dev.table.TableData;
 import ghostface.dev.table.Table;
-import ghostface.dev.table.column.Column;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.Collection;
 
-public interface Element extends UnmodifiableContent<@NotNull Data<?>> {
+public interface Element extends UnmodifiableContent<@NotNull TableData<?>> {
 
     @NotNull String getId();
 
     @NotNull Table getTable();
 
     /**
-     * @throws IllegalArgumentException if the column does not exist
+     * @throws InvalidColumnException if the {@code column id} does not present
      * */
-    <E> @Nullable E get(@NotNull Column<E> column) throws IllegalArgumentException;
+    <E> @Nullable E get(@NotNull String columnId) throws InvalidColumnException;
 
     /**
      * @throws InvalidColumnException if the {@code columnId} does not exist in Table#getColumns.
@@ -30,5 +29,5 @@ public interface Element extends UnmodifiableContent<@NotNull Data<?>> {
     <E> void set(@NotNull String columnId, @Nullable E value) throws InvalidColumnException, ColumnException;
 
     @Override
-    @Unmodifiable @NotNull Collection<@NotNull Data<?>> toCollection();
+    @Unmodifiable @NotNull Collection<@NotNull TableData<?>> toCollection();
 }

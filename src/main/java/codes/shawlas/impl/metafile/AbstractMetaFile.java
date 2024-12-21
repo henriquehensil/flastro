a21cd8af4dea8e06a2f16705c7619d1bfabeb92d
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-21 12:43:06 -0300

    moved AbstractMetaFile

--- src/main/java/codes/shawlas/impl/metafile/AbstractMetaFile.java
+++ src/main/java/codes/shawlas/impl/metafile/AbstractMetaFile.java
@@ -1,6 +1,9 @@
-package codes.shawlas.file;
+package codes.shawlas.impl.metafile;
 
+import codes.shawlas.exception.file.InvalidFileAttributesException;
+import codes.shawlas.file.MetaFile;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.jetbrains.annotations.Blocking;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -13,18 +16,54 @@ import java.nio.file.attribute.FileTime;
 
 public abstract class AbstractMetaFile implements MetaFile {
 
-    protected final @NotNull BasicFileAttributes attributes;
-    private final @NotNull File file;
+    // Static initializers
 
-    // Modules
+    static @NotNull JsonObject jsonProvider(@NotNull MetaFile metaFile) {
+        @NotNull JsonObject general = new JsonObject();
+        general.addProperty("name", metaFile.getPath().getFileName().toString());
+        general.addProperty("key", metaFile.getKey());
+        general.addProperty("size", metaFile.getData().size());
+        general.addProperty("path", metaFile.getPath().toString());
+
+        @NotNull JsonObject times = new JsonObject();
+        times.addProperty("created", metaFile.getTimes().getCreated().toString());
+        times.addProperty("modified", metaFile.getTimes().getModified().toString());
+        times.addProperty("access", metaFile.getTimes().getAccess().toString());
+
+        @NotNull JsonObject permissions = new JsonObject();
+        permissions.addProperty("readable", metaFile.getPermissions().isFullyReadable());
+        permissions.addProperty("writable", metaFile.getPermissions().isFullyWritable());
+        permissions.addProperty("executable", metaFile.getPermissions().isFullyExecutable());
+
+        @NotNull JsonObject object = new JsonObject();
+        object.add("general", general);
+        object.add("times", times);
+        object.add("permissions", permissions);
+
+        return object;
+    }
+
+    // Objects
+
+    protected final @NotNull BasicFileAttributes attributes;
+
+    private final @NotNull File file;
     private final @NotNull FileData data = new FileDataImpl();
     private final @NotNull FileTimes times = new FileTimesImpl();
     private final @NotNull FilePermissions permissions = new FilePermissionsImpl();
 
-    // Constructor
-    protected <T extends BasicFileAttributes> AbstractMetaFile(@NotNull File file, @NotNull T attributes) {
-        this.file = file;
-        this.attributes = attributes;
+    /**
+     * @throws IOException if an I/O errors occurs
+     * @throws InvalidFileAttributesException  if an attributes of the given type are not supported
+     * @throws SecurityException if the security manager of this file prevents any operation
+     * */
+    protected <T extends BasicFileAttributes> AbstractMetaFile(@NotNull File file, @NotNull Class<T> attribute) throws IOException, InvalidFileAttributesException, SecurityException {
+        try {
+            this.attributes = Files.readAttributes(file.toPath(), attribute);
+            this.file = file;
+        } catch (UnsupportedOperationException e) {
+            throw new InvalidFileAttributesException(e.getMessage());
+        }
     }
 
     public abstract @NotNull JsonElement serialize();

38a866511489bc986924deaf36c007a494533b8b
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-23 23:54:14 -0300

    improved code

--- src/main/java/codes/shawlas/table/Element.java
+++ src/main/java/codes/shawlas/table/Element.java
@@ -1,6 +1,5 @@
 package codes.shawlas.table;
 
-import codes.shawlas.content.UnmodifiableContent;
 import codes.shawlas.exception.column.ColumnException;
 import codes.shawlas.exception.column.InvalidColumnException;
 import org.jetbrains.annotations.NotNull;
@@ -9,23 +8,23 @@ import org.jetbrains.annotations.Unmodifiable;
 
 import java.util.Collection;
 
-public interface Element extends UnmodifiableContent<@NotNull TableData<?>> {
+public interface Element {
 
     @NotNull String getId();
 
     @NotNull Table getTable();
 
     /**
-     * @throws InvalidColumnException if the {@code column id} does not present
+     * @throws InvalidColumnException if the {@code column} does not present
      * */
-    <E> @Nullable E get(@NotNull String columnId) throws InvalidColumnException;
+    <E> @Nullable E get(@NotNull Column<E> column) throws InvalidColumnException;
 
     /**
-     * @throws InvalidColumnException if the {@code columnId} does not exist in Table#getColumns.
+     * @throws InvalidColumnException if the {@code column} does not exist in Table#getColumns.
      * @throws ColumnException if the {@code value} is null and the column is either non-nullable or a key column.
      * */
-    <E> void set(@NotNull String columnId, @Nullable E value) throws InvalidColumnException, ColumnException;
+    <E> void set(@NotNull Column<E> column, @Nullable E value) throws InvalidColumnException, ColumnException;
 
-    @Override
-    @Unmodifiable @NotNull Collection<@NotNull TableData<?>> toCollection();
+    @Unmodifiable @NotNull Collection<@NotNull TableData<?>> getData();
+
 }

3d2af15e65e8e763d1940453ceb64212c5515c14
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-28 23:52:39 -0300

    added InputMessageException

---
+++ message/src/main/java/codes/shawlas/data/exception/InputMessageException.java
@@ -0,0 +1,7 @@
+package codes.shawlas.data.exception;
+
+public class InputMessageException extends Exception {
+    public InputMessageException(String message) {
+        super(message);
+    }
+}

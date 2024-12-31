a4e010a8fc96c6a6828600131ac560eaffa6c898
Henrique Gustavo <henriquedev6@gmail.com>
2024-12-31 20:26:09 -0300

    improved tests

--- commons/src/test/java/MessageTests.java
+++ commons/src/test/java/MessageTests.java
@@ -7,26 +7,24 @@ import org.junit.jupiter.api.Test;
 
 public final class MessageTests {
 
-    @NotNull String @NotNull [] valid = {
+    private final @NotNull String @NotNull [] valid = {
             "GET FILE\r\nIDENTIFIER: 12\r\n\r\n{\"path\": \"/root/test.json\"}",
-            "CREATE COLUMN\r\nIDENTIFIER: 32\r\n\r\n{\"table\": \"6\", \"name\": \"test\"}",
+            "FINISHED\r\nCREATE COLUMN\r\nIDENTIFIER: 32\r\n\r\n{\"table\": \"6\", \"name\": \"test\"}",
             "DELETE NEST\r\nIDENTIFIER: 54\r\n\r\n{\"father\": \"null\", \"id\": \"4756\"}",
-            "UPDATE ELEMENT\r\nIDENTIFIER: 24\r\n\r\n{\"table\": \"2\", \"element\": \"254\", \"value\": \"test\", \"column\": \"nickname\"}"
+            "FINISHED\r\nUPDATE ELEMENT\r\nIDENTIFIER: 24\r\n\r\n{\"table\": \"2\", \"element\": \"254\", \"value\": \"test\", \"column\": \"nickname\"}"
     };
-
-    @NotNull String @NotNull [] invalid = {
+    private final @NotNull String @NotNull [] invalid = {
             "TAKE FILE\r\nIDENTIFIER: 12\r\n\r\n{\"path\": \"/root/test.json\"}",
             "NEW COLUMN\r\nIDENTIFIER: 32\r\n\r\n{\"table\": \"6\", \"name\": \"test\"}",
             "REMOVE NEST\r\nIDENTIFIER: 54\r\n\r\n{\"father\": \"null\", \"id\": \"4756\"}",
             "SET ELEMENT\r\nIDENTIFIER: 24\r\n\r\n{\"table\": \"2\", \"element\": \"254\", \"value\": \"test\", \"column\": \"nickname\"}",
 
             "GET FILE\r\nIDENTIFIER: -12\r\n\r\n{\"path\": \"/root/test.json\"}",
-            "CREATE COLUMN\r\nIDENTIFIER: -32\r\n\r\n{\"table\": \"6\", \"name\": \"test\"}",
+            "FINISHED\r\nCREATE COLUMN\r\nIDENTIFIER: -32\r\n\r\n{\"table\": \"6\", \"name\": \"test\"}",
             "DELETE NEST\r\nIDENTIFIER: -54\r\n\r\n{\"father\": \"null\", \"id\": \"4756\"}",
             "UPDATE ELEMENT\r\nIDENTIFIER: -24\r\n\r\n{\"table\": \"2\", \"element\": \"254\", \"value\": \"test\", \"column\": \"nickname\"}",
     };
-
-    @NotNull String @NotNull [] invalidJsons = {
+    private final @NotNull String @NotNull [] invalidJsons = {
             "GET FILE\r\nIDENTIFIER: 12\r\n\r\n{\"path\" \"/root/test.json\"}",
             "CREATE COLUMN\r\nIDENTIFIER: 32\r\n\r\n{table \"6\", \"name\": \"test\"}",
             "DELETE NEST\r\nIDENTIFIER: 54\r\n\r\n{\"father\": \"null\", \"id\": \"4756\"",
@@ -52,6 +50,22 @@ public final class MessageTests {
         for (@NotNull String invalid : invalidJsons) {
             Assertions.assertFalse(MessageStructure.isValid(invalid));
             Assertions.assertThrows(MessageParseException.class, () -> MessageStructure.parse(invalid));
+        }
+    }
+
+    @Test
+    public void testsFinished() {
+        @NotNull String @NotNull [] validFinished = valid;
+        for (@NotNull String valid : validFinished) {
+            valid = valid.startsWith("FINISHED") ? valid : "FINISHED\r\n" + valid;
+            Assertions.assertTrue(MessageStructure.isValid(valid));
+            Assertions.assertTrue(MessageStructure.isValidHeader(valid.split("\r\n\r\n")[0]));
+            try {
+                @NotNull MessageStructure structure = MessageStructure.parse(valid.split("\r\n\r\n")[0], new JsonObject());
+                Assertions.assertTrue(structure.isFinished());
+            } catch (Throwable e) {
+                Assertions.fail();
+            }
         }
     }
 }

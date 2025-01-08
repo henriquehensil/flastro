6211c77324812d464db8723e5ab14f43930deb08
Henrique Gustavo <henriquedev6@gmail.com>
2025-01-07 21:12:40 -0300

    added TableStorage

---
+++ core/src/main/java/codes/shawlas/data/table/TableStorage.java
@@ -0,0 +1,27 @@
+package codes.shawlas.data.table;
+
+import codes.shawlas.data.Database;
+import codes.shawlas.data.exception.table.TableAlreadyExistsException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public interface TableStorage {
+
+    @NotNull Tables getTables();
+
+    // Classes
+
+    interface Tables extends Iterable<@NotNull Table> {
+
+        /**
+         * @throws TableAlreadyExistsException if table {@code name} is already in use
+         * */
+        @NotNull Table create(@NotNull String name) throws TableAlreadyExistsException;
+
+        boolean remove(@NotNull String name);
+
+        @NotNull Optional<@NotNull Table> get(@NotNull String name);
+
+    }
+}

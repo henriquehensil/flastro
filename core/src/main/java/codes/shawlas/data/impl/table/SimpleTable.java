9159a1e7f87f63b051149f92d21d5ffd8e59ee39
Henrique Gustavo <henriquedev6@gmail.com>
2025-01-08 22:23:35 -0300

    added equals and hashcode

--- core/src/main/java/codes/shawlas/data/impl/table/SimpleTable.java
+++ core/src/main/java/codes/shawlas/data/impl/table/SimpleTable.java
@@ -2,6 +2,9 @@ package codes.shawlas.data.impl.table;
 
 import codes.shawlas.data.table.Table;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 final class SimpleTable implements Table {
 
@@ -33,5 +36,22 @@ final class SimpleTable implements Table {
     @Override
     public @NotNull SimpleColumns getColumns() {
         return columns;
+    }
+
+    @Override
+    public boolean equals(@Nullable Object o) {
+        if (o instanceof Table) {
+            final @NotNull Table that = (Table) o;
+            return
+                    Objects.equals(name.toLowerCase(), that.getName().toLowerCase()) &&
+                    Objects.equals(columns.getTable().name.toLowerCase(), that.getColumns().getTable().getName().toLowerCase()) &&
+                    Objects.equals(elements.getTable().name.toLowerCase(), that.getElements().getTable().getName().toLowerCase());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name.toLowerCase());
     }
 }

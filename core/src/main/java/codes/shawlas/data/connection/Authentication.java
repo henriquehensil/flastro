dde365ce7aa838b17ef0b2ce854e9d60bc2bc00b
shawlas <hgsw.contato@gmail.com>
2025-01-30 01:38:08 -0300

    added Authentication#getKey

--- core/src/main/java/codes/shawlas/data/connection/Authentication.java
+++ core/src/main/java/codes/shawlas/data/connection/Authentication.java
@@ -2,6 +2,7 @@ package codes.shawlas.data.connection;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -12,6 +13,40 @@ public interface Authentication {
 
     static @NotNull Authentication create(@NotNull InetSocketAddress address, @NotNull String username, @NotNull String password) {
         return new Authentication() {
+            @Override
+            public @UnknownNullability Object getKey() {
+                return null;
+            }
+
+            @Override
+            public @NotNull InetAddress getAddress() {
+                return address.getAddress();
+            }
+
+            @Override
+            public @Range(from = 0, to = 65535) int getPort() {
+                return address.getPort();
+            }
+
+            @Override
+            public @NotNull String getUsername() {
+                return username;
+            }
+
+            @Override
+            public @NotNull String getPassword() {
+                return password;
+            }
+        };
+    }
+
+    static @NotNull Authentication create(@NotNull Object key, @NotNull InetSocketAddress address, @NotNull String username, @NotNull String password) {
+        return new Authentication() {
+            @Override
+            public @UnknownNullability Object getKey() {
+                return key;
+            }
+
             @Override
             public @NotNull InetAddress getAddress() {
                 return address.getAddress();
@@ -35,6 +70,8 @@ public interface Authentication {
     }
 
     // Objects
+
+    @UnknownNullability Object getKey();
 
     @NotNull InetAddress getAddress();
 

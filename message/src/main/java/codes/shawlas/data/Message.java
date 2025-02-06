e6d4b5efcab53f3a288c3e4b64fdc354acece1bb
shawlas <hgsw.contato@gmail.com>
2025-02-06 03:45:05 -0300

    improved system

--- message/src/main/java/codes/shawlas/data/Message.java
+++ message/src/main/java/codes/shawlas/data/Message.java
@@ -2,78 +2,32 @@ package codes.shawlas.data;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.Range;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Objects;
+import java.nio.channels.WritableByteChannel;
 
 public sealed interface Message permits Message.Input, Message.Output {
 
-    @Range(from = 0, to = Long.MAX_VALUE) int getId();
+    @Nullable Request getRequest();
 
-    byte @NotNull [] getBytes();
+    @NotNull Object getBody();
 
     // Classes
 
-    non-sealed abstract class Input implements Message {
+    non-sealed interface Input extends Message {
 
-        private final int id;
+        @NotNull Object execute();
 
-        public Input(int id) {
-            if (id <= 0) throw new IllegalArgumentException("Invalid id");
-            this.id = id;
-        }
-
-        @Override
-        public final @Range(from = 0, to = Long.MAX_VALUE) int getId() {
-            return id;
-        }
-
-        /**
-         * @return  The output processed or null if the process is not needed
-         * */
-        public abstract @Nullable Output process();
-
-        @Override
-        public final boolean equals(@Nullable Object object) {
-            if (object == null || getClass() != object.getClass()) return false;
-            @NotNull Input input = (Input) object;
-            return id == input.id;
-        }
-
-        @Override
-        public final int hashCode() {
-            return Objects.hashCode(id);
-        }
     }
 
-    non-sealed abstract class Output implements Message {
+    non-sealed interface Output extends Message {
 
-        private final int id;
+        @NotNull Object getStatus();
 
-        public Output(int id) {
-            if (id <= 0) throw new IllegalArgumentException("Invalid id");
-            this.id = id;
-        }
+        void write(@NotNull OutputStream output) throws IOException;
 
-        @Override
-        public final @Range(from = 0, to = Long.MAX_VALUE) int getId() {
-            return id;
-        }
+        void write(@NotNull WritableByteChannel output) throws IOException;
 
-        public abstract void write(@NotNull OutputStream output) throws IOException;
-
-        @Override
-        public final boolean equals(@Nullable Object object) {
-            if (object == null || getClass() != object.getClass()) return false;
-            @NotNull Output output = (Output) object;
-            return id == output.id;
-        }
-
-        @Override
-        public final int hashCode() {
-            return Objects.hashCode(id);
-        }
     }
 }

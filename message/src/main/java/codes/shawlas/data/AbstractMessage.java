5018165b147f5b6f8beefe7b709ab51f7d5b0a99
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-02-10 00:24:08 -0300

    improved code

--- message/src/main/java/codes/shawlas/data/AbstractMessage.java
+++ message/src/main/java/codes/shawlas/data/AbstractMessage.java
@@ -3,24 +3,42 @@ package codes.shawlas.data;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public non-sealed abstract class AbstractMessage implements Message {
+public sealed abstract class AbstractMessage implements Message permits AbstractMessage.Input, AbstractMessage.Output {
 
     private final @NotNull String id;
     private final @Nullable String correlationId;
 
-    public AbstractMessage(@NotNull String id, @Nullable String correlationId) {
+    protected AbstractMessage(@NotNull String id, @Nullable String correlationId) {
         this.id = id;
         this.correlationId = correlationId;
     }
 
     @Override
-    public @NotNull String getId() {
+    public final @NotNull String getId() {
         return id;
     }
 
     @Override
-    public @Nullable String getCorrelationId() {
+    public final @Nullable String getCorrelationId() {
         return correlationId;
+    }
+
+    // Classes
+
+    public non-sealed abstract static class Input extends AbstractMessage implements Message.Input {
+
+        public Input(@NotNull String id, @Nullable String correlationId) {
+            super(id, correlationId);
+        }
+
+    }
+
+    public non-sealed abstract static class Output extends AbstractMessage implements Message.Output {
+
+        public Output(@NotNull String id, @Nullable String correlationId) {
+            super(id, correlationId);
+        }
+
     }
 
 }

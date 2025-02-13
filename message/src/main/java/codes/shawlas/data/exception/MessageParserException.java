d9fb2d1e7e4d7bb1f4e978cc938b37a2de56e207
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-02-12 23:17:40 -0300

    added Message parser

--- message/src/main/java/codes/shawlas/data/exception/MessageParserException.java
+++ message/src/main/java/codes/shawlas/data/exception/MessageParserException.java
@@ -1,13 +1,7 @@
 package codes.shawlas.data.exception;
 
-import org.jetbrains.annotations.NotNull;
-
 public class MessageParserException extends Exception {
-    public MessageParserException(@NotNull String message) {
-        super(message);
-    }
-
-    public MessageParserException(@NotNull String message, @NotNull Throwable cause) {
-        super(message, cause);
-    }
+  public MessageParserException(String message) {
+    super(message);
+  }
 }
--- message/src/main/java/codes/shawlas/data/message/MessageExecutor.java
+++
@@ -1,9 +0,0 @@
-package codes.shawlas.data.message;
-
-import codes.shawlas.data.exception.MessageExecutionException;
-
-public interface MessageExecutor {
-
-    void execute() throws MessageExecutionException;
-
-}

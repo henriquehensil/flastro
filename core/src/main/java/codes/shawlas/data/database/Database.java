119e7a5a29ef19b119e10693734088c74bd6c31c
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-02-16 22:32:38 -0300

    improved codes

--- core/src/main/java/codes/shawlas/data/database/Database.java
+++ core/src/main/java/codes/shawlas/data/database/Database.java
@@ -1,7 +1,9 @@
 package codes.shawlas.data.database;
 
+import codes.shawlas.data.impl.FileStorage;
 import codes.shawlas.data.storage.Storage;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -16,6 +18,8 @@ public interface Database {
 
     interface Storages {
 
+        @NotNull FileStorage getFileStorage();
+
         /**
          * @throws IllegalArgumentException if the id doest not exists
          * */
@@ -27,11 +31,65 @@ public interface Database {
 
     interface Authentication {
 
+        static @NotNull Authentication create(@NotNull InetAddress address, int port) {
+            if (port < 0 || port > 65535) throw new IllegalArgumentException("Illegal port value");
+
+            return new Authentication() {
+                @Override
+                public @NotNull InetAddress getAddress() {
+                    return address;
+                }
+
+                @Override
+                public @NotNull String getUsername() {
+                    return "";
+                }
+
+                @Override
+                public @NotNull String getPassword() {
+                    return "";
+                }
+
+                @Override
+                public @Range(from = 0, to = 65535) int getPort() {
+                    return port;
+                }
+            };
+        }
+
+        static @NotNull Authentication create(@NotNull InetAddress address, int port, @NotNull String username, @NotNull String password) {
+            if (port < 0 || port > 65535) throw new IllegalArgumentException("Illegal port value");
+
+            return new Authentication() {
+                @Override
+                public @NotNull InetAddress getAddress() {
+                    return address;
+                }
+
+                @Override
+                public @NotNull String getUsername() {
+                    return password;
+                }
+
+                @Override
+                public @NotNull String getPassword() {
+                    return username;
+                }
+
+                @Override
+                public @Range(from = 0, to = 65535) int getPort() {
+                    return port;
+                }
+            };
+        }
+
         @NotNull InetAddress getAddress();
 
         @NotNull String getUsername();
 
         @NotNull String getPassword();
+
+        @Range(from = 0, to = 65535) int getPort();
 
     }
 
--- core/src/main/java/codes/shawlas/data/impl/FileStorage.java
+++ core/src/main/java/codes/shawlas/data/impl/FileStorage.java
@@ -16,6 +16,7 @@ public class FileStorage implements Storage {
     public FileStorage(@NotNull Path root) throws IOException {
         if (!Files.exists(root)) {
             Files.createDirectories(root);
+            Files.createDirectory(root.resolve("upload"));
         }
 
         this.root = root;
@@ -40,80 +41,119 @@ public class FileStorage implements Storage {
 
         private final @NotNull Set<@NotNull Path> paths = new HashSet<>();
 
-        public @NotNull File create(@NotNull String folders, @NotNull String name, @NotNull InputStream data) throws IOException {
-            @NotNull Path path = Paths.get(folders, name);
-            path = path.startsWith(root) ? path : root.resolve(path);
+        public @NotNull FileOutputStream createFile(@NotNull Path path) throws IOException {
+            path = format(path);
 
             synchronized (lock) {
-                if (contains(path)) {
-                    throw new FileAlreadyExistsException("The file already exists: " + folders);
-                }
-
-                if (!Files.exists(path.getParent())) {
-                    Files.createDirectory(path.getParent());
+                if (!paths.contains(path.getParent())) {
+                    Files.createDirectories(path.getParent());
                 }
 
                 Files.createFile(path);
 
-                if (Files.isDirectory(path)) {
-                    throw new IllegalArgumentException("The path already exists, but the " + name + " is not a file: " + path);
-                }
-
-                final @NotNull File file = path.toFile();
-                write(file, data);
                 paths.add(path);
-
-                return file;
+                return new FileOutputStream(path.toFile());
             }
         }
 
-        private void write(@NotNull File file, @NotNull InputStream inputStream) throws IOException {
-            if (file.isDirectory()) {
-                throw new IllegalArgumentException("File must to be a regular file: " + file.getAbsolutePath());
-            } else try (@NotNull FileOutputStream output = new FileOutputStream(file)) {
-                byte @NotNull [] bytes = new byte[8192]; // 8kb
+        public @NotNull File createDirectories(@NotNull Path path) throws IOException {
+            path = format(path);
 
-                int read;
-                while ((read = inputStream.read(bytes)) != -1) {
-                    output.write(bytes, 0, read);
-                    output.flush();
-                }
-            }
-        }
+            synchronized (lock) {
+                Files.createDirectories(path);
 
-        public boolean contains(@NotNull String path) {
-            try {
-                return contains(Paths.get(path));
-            } catch (InvalidPathException e) {
-                return false;
+                paths.add(path);
+                return path.toFile();
             }
         }
 
         public final boolean contains(@NotNull Path path) {
             synchronized (lock) {
-                return paths.contains(path.startsWith(root) ? path : root.resolve(path));
+                return paths.contains(format(path));
             }
         }
 
-        public @NotNull InputStream get(@NotNull Path path) throws IOException {
-            path = path.startsWith(root) ? path : root.resolve(path);
+        public @NotNull FileInputStream getInputStream(@NotNull Path path) throws FileNotFoundException {
+            path = format(path);
 
             synchronized (lock) {
                 if (!paths.contains(path)) {
                     throw new FileNotFoundException("The path doest not exists in the storage");
                 }
 
-                return Files.newInputStream(path);
+                return new FileInputStream(path.toFile());
             }
         }
 
-        public boolean delete(@NotNull String path) throws IOException {
-            final @NotNull Path p = path.startsWith(root.toString()) ? Paths.get(path) : root.resolve(path);
+        public @NotNull FileOutputStream getOutputStream(@NotNull Path path, boolean append) throws FileNotFoundException {
+            path = format(path);
 
             synchronized (lock) {
-                paths.remove(p);
-                return Files.deleteIfExists(p);
+                if (!paths.contains(path)) {
+                    throw new FileNotFoundException("The path doest not exists in the storage: " + path);
+                }
+
+                return new FileOutputStream(path.toFile(), append);
             }
+        }
+
+        public boolean isDirectory(@NotNull Path path) {
+            return contains(path) && Files.isDirectory(path);
+        }
+
+        public boolean delete(@NotNull Path path) throws IOException {
+            path = format(path);
+
+            synchronized (lock) {
+                paths.remove(path);
+                return Files.deleteIfExists(path);
+            }
+        }
+
+        public @NotNull String [] getFolders(@NotNull Path path) throws IOException {
+            path = format(path);
+
+            if (!contains(path)) {
+                throw new FileNotFoundException("The path doest not exists in the storage: " + path);
+            } else synchronized (lock) {
+                try (@NotNull DirectoryStream<@NotNull Path> dir = Files.newDirectoryStream(path)) {
+
+                    @NotNull List<@NotNull String> folders = new LinkedList<>();
+
+                    for (@NotNull Path file : dir) {
+                        if (Files.isDirectory(file)) {
+                            folders.add(file.getFileName().toString());
+                        }
+                    }
+
+                    return folders.toArray(new String[0]);
+                }
+            }
+        }
+
+        public @NotNull String [] getFiles(@NotNull Path path) throws IOException {
+            path = format(path);
+
+            if (!contains(path)) {
+                throw new FileNotFoundException("The path doest not exists in the storage: " + path);
+            } else synchronized (lock) {
+                try (@NotNull DirectoryStream<@NotNull Path> dir = Files.newDirectoryStream(path)) {
+
+                    @NotNull List<@NotNull String> folders = new LinkedList<>();
+
+                    for (@NotNull Path file : dir) {
+                        if (!Files.isDirectory(file)) {
+                            folders.add(file.getFileName().toString());
+                        }
+                    }
+
+                    return folders.toArray(new String[0]);
+                }
+            }
+        }
+
+        private @NotNull Path format(@NotNull Path path) {
+            return path.startsWith(root.toString()) ? path : root.resolve(path);
         }
 
         public void deleteAll() {

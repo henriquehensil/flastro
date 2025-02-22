2f12ce0cfce2b6de98c0292b5a991b0cd7d1ecd0
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-02-21 23:04:40 -0300

    added Message Interface

---
+++ server/src/main/java/codes/shawlas/data/message/Message.java
@@ -0,0 +1,32 @@
+package codes.shawlas.data.message;
+
+import codes.shawlas.data.database.Database;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public sealed interface Message permits Message.Input, Message.Output {
+
+    byte getCode();
+
+    @NotNull UUID getId();
+
+    @NotNull OffsetDateTime getTime();
+
+    // Classes
+
+    non-sealed interface Input extends Message {
+
+        default @NotNull MessageExecutor getExecutor(@NotNull Database database) {
+            return MessageExecutor.from(this, database);
+        }
+
+    }
+
+    non-sealed interface Output extends Message {
+
+        @NotNull MessageBuffer serialize();
+
+    }
+}

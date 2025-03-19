6816283b3ebabb1f89d9a51de63d021584bca3de
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-03-18 23:18:56 -0300

    Added MetaFile

---
+++ api/src/main/java/dev/hensil/flastro/api/file/MetaFile.java
@@ -0,0 +1,38 @@
+package dev.hensil.flastro.api.file;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.attribute.FileTime;
+
+public interface MetaFile {
+
+    @NotNull MetaFile.Data getData();
+
+    @NotNull MetaFile.Time getTime();
+
+    // Classes
+
+    interface Data {
+
+        @NotNull FileInputStream getInputStream() throws IOException;
+
+        @NotNull FileOutputStream getOutputStream(boolean append) throws IOException;
+
+        @Range(from = 0, to = Long.MAX_VALUE) long size();
+
+    }
+
+    interface Time {
+
+        @NotNull FileTime getLastModified();
+
+        @NotNull FileTime getLastAccess();
+
+        @NotNull FileTime getCreation();
+
+    }
+}

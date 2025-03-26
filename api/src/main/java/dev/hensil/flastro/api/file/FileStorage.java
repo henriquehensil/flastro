48469e13d7e48726338a215263e11d8afa8cde2f
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-03-25 23:14:25 -0300

    improved system

--- api/src/main/java/dev/hensil/flastro/api/file/FileStorage.java
+++ api/src/main/java/dev/hensil/flastro/api/file/FileStorage.java
@@ -1,9 +1,10 @@
 package dev.hensil.flastro.api.file;
 
+import dev.hensil.flastro.api.exception.FileStorageException;
 import dev.hensil.flastro.api.storage.Storage;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.file.Path;
 
 public interface FileStorage extends Storage {
@@ -12,17 +13,39 @@ public interface FileStorage extends Storage {
 
     @NotNull Manager getManager();
 
+    @NotNull Data getData();
+
     // Classes
 
     interface Manager {
 
-        @NotNull MetaFile create(@NotNull Path path) throws IOException;
+        @NotNull MetaFile createFile(@NotNull Path path) throws FileStorageException;
 
-        @NotNull MetaFile get(@NotNull Path path) throws FileNotFoundException;
+        @NotNull MetaFile createFolder(@NotNull Path path) throws FileStorageException;
+
+        void move(@NotNull Path path, @NotNull Path target) throws FileStorageException;
+
+        void copy(@NotNull Path path, @NotNull Path target) throws FileStorageException;
+
+        @NotNull MetaFile get(@NotNull Path path) throws FileStorageException;
 
         boolean delete(@NotNull Path path);
 
         boolean contains(@NotNull Path path);
+
+    }
+
+    interface Data {
+
+        int read(@NotNull Path path, @NotNull ByteBuffer buffer) throws FileStorageException;
+
+        void write(@NotNull Path path, @NotNull ByteBuffer buffer) throws FileStorageException;
+
+        boolean decompress(@NotNull Path path);
+
+        boolean compress(@NotNull Path path);
+
+        void clear(@NotNull Path path);
 
     }
 }

1611f51c3ee746687fd8a91db278e6902a18a228
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-04-01 22:48:57 -0300

    rename module name

--- api/src/main/java/dev/hensil/flastro/api/exception/NoSuchUserException.java
+++
@@ -1,9 +0,0 @@
-package dev.hensil.flastro.api.exception;
-
-import java.util.NoSuchElementException;
-
-public class NoSuchUserException extends NoSuchElementException {
-    public NoSuchUserException(String message) {
-        super(message);
-    }
-}
--- api/src/main/java/dev/hensil/flastro/api/exception/NoSuchUserPermissionsException.java
+++
@@ -1,9 +0,0 @@
-package dev.hensil.flastro.api.exception;
-
-import java.util.NoSuchElementException;
-
-public class NoSuchUserPermissionsException extends NoSuchElementException {
-    public NoSuchUserPermissionsException(String message) {
-        super(message);
-    }
-}

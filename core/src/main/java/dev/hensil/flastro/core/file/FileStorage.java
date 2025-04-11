41fd939a7e1864c4802e399e8c515c762e8e245d
Henrique Shawlas Gustavo <hgsw.contato@gmail.com>
2025-04-10 22:28:08 -0300

    improved system..

--- core/src/main/java/dev/hensil/flastro/core/file/FileStorage.java
+++ core/src/main/java/dev/hensil/flastro/core/file/FileStorage.java
@@ -2,80 +2,89 @@ package dev.hensil.flastro.core.file;
 
 import dev.hensil.flastro.core.storage.Storage;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileDescriptor;
 import java.util.Collection;
 
 import java.io.IOException;
 import java.nio.file.NotDirectoryException;
-import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.NoSuchFileException;
-
-import dev.hensil.flastro.core.exception.IllegalOptionsException;
+import java.nio.file.FileAlreadyExistsException;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface FileStorage extends Storage {
 
     @NotNull Manager getManager();
 
+    @Override
+    default byte getCode() {
+        return 0;
+    }
+
     // Classes
 
+    // todo FilePermissionException and another settings exception
     interface Manager {
 
-        // Objects
+        /**
+         *
+         * @param folder the folder id present in {@link FileRecord#getId()}.
+         *
+         * @return A recent file record
+         *
+         * @throws IllegalArgumentException if the {@code name} is an invalid file name (optional specific operation)
+         * @throws NoSuchFileException if the {@code folder} id doest not exists
+         * @throws NotDirectoryException if the {@code folder} is present, but is not a folder
+         * @throws FileAlreadyExistsException when the {@code name} already is present in this folder
+         * @throws IOException if another specific I/O error occurs
+         * */
+        @NotNull FileRecord createFile(@NotNull String folder, @NotNull String name, boolean mkdirs) throws IOException;
 
-        default @NotNull Path getRootPath() {
-            return Paths.get("/");
-        }
+        /**
+         *
+         * @param folder the folder id present in {@link FileRecord#getId()}.
+         *
+         * @return A recent folder record
+         *
+         * @throws IllegalArgumentException if the {@code name} is an invalid folder name (optional specific operation)
+         * @throws NoSuchFileException if the {@code folder} id doest not exists.
+         * @throws NotDirectoryException if the {@code folder} is present, but is not a folder.
+         * @throws FileAlreadyExistsException when the {@code name} already is present in this folder
+         * @throws IOException if another specific I/O error occurs.
+         * */
+        @NotNull FolderRecord createFolder(@NotNull String folder, @NotNull String name, boolean mkdirs) throws IOException;
 
-        @NotNull MetaFile createFile(@NotNull Path parents, @NotNull String name, boolean mkdirs) throws IOException;
-
-        @NotNull MetaFile createFolder(@NotNull Path parents, @NotNull String name, boolean mkdirs) throws IOException;
-
-        @NotNull Optional<@NotNull MetaFile> get(@NotNull URI uri);
+        @NotNull Optional<@NotNull FileRecord> get(@NotNull String fileId);
 
         /**
          * @throws NoSuchFileException if the file doest not exists.
-         * @throws IllegalArgumentException if the file exists, but is a folder.
          * @throws IOException if another error occurs.
          * */
-        void deleteFile(@NotNull URI uri) throws IOException;
+        void delete(@NotNull String file) throws IOException;
 
         /**
-         * @throws NoSuchFileException if the folder doest not exists.
-         * @throws NotDirectoryException if the file is not a folder.
-         * @throws DirectoryNotEmptyException if the folder is not empty and the {@code onlyIfIsEmpty} parameter is true.
-         * @throws IOException if another error occurs.
-         * */
-        void deleteFolder(@NotNull URI uri, boolean onlyIfIsEmpty) throws IOException;
-
-        /**
-         * @throws IllegalOptionsException if the set contains an illegal option combination (optional specific operation).
-         * @throws NoSuchFileException if the file or folder given by the both URI parameter doest not exists.
+         * @throws NoSuchFileException if one of the files does not exist.
+         * @throws NotDirectoryException if the {@code target} file exist but is not a folder.
          * @throws IOException if another I/O error occurs.
          * */
-        void move(@NotNull URI from, @NotNull URI target, @Nullable Set<@NotNull MoveOption> moveOption) throws IOException;
+        void move(@NotNull String file, @NotNull String fileTarget) throws IOException;
 
         /**
-         * @throws IllegalOptionsException if the set contains an illegal option combination (optional specific operation).
-         * @throws NoSuchFileException if the file or folder given by the both URI parameter doest not exists.
+         * @throws NoSuchFileException if one of the files does not exist.
+         * @throws NotDirectoryException if the {@code fileTarget} file exist but is not a folder.
          * @throws IOException if another I/O error occurs.
          * */
-        void copy(@NotNull URI from, @NotNull URI target, @Nullable Set<@NotNull CopyOption> copyOption) throws IOException;
+        void copy(@NotNull String file, @NotNull String fileTarget) throws IOException;
 
         /**
          * @throws IllegalArgumentException if the {@code newName} is illegal (optional specific operation).
          * @throws NoSuchFileException if the file doest not exists.
          * @throws IOException if a I/O e error occurs
          * */
-        void rename(@NotNull URI uri, @NotNull String newName) throws IOException;
+        void rename(@NotNull String file, @NotNull String newName) throws IOException;
 
-        @NotNull Collection<? extends @NotNull MetaFile> getFiles(@NotNull Path location) throws IOException;
+        @NotNull Collection<? extends @NotNull FileDescriptor> getFiles(@NotNull String folder) throws IOException;
 
     }
 
@@ -85,42 +94,42 @@ public interface FileStorage extends Storage {
     *
     * */
 
-    interface MoveOption {
-
-        @NotNull MoveOption CREATE_IF_NOT_EXIST = Standard.CREATE;
-
-        @NotNull MoveOption ONLY_TARGET_IS_EMPTY = Standard.ONLY_TARGET_EMPTY;
-
-        @NotNull MoveOption REPLACE_EXISTING = Standard.REPLACE;
-
-        // Classes
-
-        enum Standard implements MoveOption {
-            CREATE,
-            ONLY_TARGET_EMPTY,
-            REPLACE
-            ;
-        }
-    }
-
-    interface CopyOption {
-
-        @NotNull CopyOption CREATE_IF_NOT_EXIST = Standard.CREATE;
-
-        @NotNull CopyOption COPY_ATTRIBUTES = Standard.ATTRIBUTES;
-
-        @NotNull CopyOption ONLY_TARGET_IS_EMPTY = Standard.ONLY_TARGET_EMPTY;
-
-        @NotNull CopyOption REPLACE_EXISTING = Standard.REPLACE;
-
-        // Classes
-
-        enum Standard implements CopyOption {
-            CREATE,
-            ATTRIBUTES,
-            ONLY_TARGET_EMPTY,
-            REPLACE,
-            ;
-        }
-    }
+//    interface MoveOption {
+//
+//        @NotNull MoveOption CREATE_IF_NOT_EXIST = Standard.CREATE;
+//
+//        @NotNull MoveOption ONLY_TARGET_IS_EMPTY = Standard.ONLY_TARGET_EMPTY;
+//
+//        @NotNull MoveOption REPLACE_EXISTING = Standard.REPLACE;
+//
+//        // Classes
+//
+//        enum Standard implements MoveOption {
+//            CREATE,
+//            ONLY_TARGET_EMPTY,
+//            REPLACE
+//            ;
+//        }
+//    }
+//
+//    interface CopyOption {
+//
+//        @NotNull CopyOption CREATE_IF_NOT_EXIST = Standard.CREATE;
+//
+//        @NotNull CopyOption COPY_ATTRIBUTES = Standard.ATTRIBUTES;
+//
+//        @NotNull CopyOption ONLY_TARGET_IS_EMPTY = Standard.ONLY_TARGET_EMPTY;
+//
+//        @NotNull CopyOption REPLACE_EXISTING = Standard.REPLACE;
+//
+//        // Classes
+//
+//        enum Standard implements CopyOption {
+//            CREATE,
+//            ATTRIBUTES,
+//            ONLY_TARGET_EMPTY,
+//            REPLACE,
+//            ;
+//        }
+//    }
 }
